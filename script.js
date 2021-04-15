1cb29aee6ba78913cf331e71e01078bcc5e19d31
Manuel <carlosd99m@gmail.com>
2021-04-15 14:05:14 -0600

    Operaciones de potencia y multiplicacion lista :fire:

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("c").addEventListener("click", function () {
+    var op = document.getElementById("op").value,
+      n1 = document.getElementById("n1").value,
+      n2 = document.getElementById("n2").value,
+      operacion = op == 1 ? Math.pow(n1, n2) : n1 * n2;
+    document.getElementById("r").innerHTML = operacion;
+  });
+});
