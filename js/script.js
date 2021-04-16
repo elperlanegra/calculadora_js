72d0f78341d3539cbad1a5c0a7825d76cd5a66e0
Manuel <carlosd99m@gmail.com>
2021-04-15 18:06:05 -0600

    operaciones matematicas completadas

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-window.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("c").addEventListener("click", function () {
-    
-    var op = document.getElementById("op").value,
- 
-      n1 = document.getElementById("n1").value,
-      n2 = document.getElementById("n2").value,
- 
-      // operacion = op == 1 ? Math.pow(n1, n2) : n1 * n2;
+// Codigo JavaScript
 
-      operacion = op == 1 == n1 + n2;
-      operacion = op == 2 == n1 - n2; 
-      operacion = op == 3 == n1 * n2; 
-      operacion = op == 4 == n1 / n2; 
+function calculator() {
+  // Las variables num1, num2 manipularán los valores ingresados ​​por el usuario
+  let num1 = parseInt(document.getElementById("numero1").value);
+  let num2 = parseInt(document.getElementById("numero2").value);
 
- 
-      document.getElementById("r").innerHTML = operacion;
-  
-  });
-});
+  // Definamos que tipo de operación tenemos que hacer
+  let operation = document.getElementById("operaciones").value;
+
+  if (operation === "suma") {
+    document.getElementById("resultado").value = num1 + num2;
+  }
+
+  if (operation === "resta") {
+    document.getElementById("resultado").value = num1 - num2;
+  }
+
+  if (operation === "division") {
+    document.getElementById("resultado").value = num1 / num2;
+  }
+
+  if (operation === "mult") {
+    document.getElementById("resultado").value = num1 * num2;
+  }
+}
